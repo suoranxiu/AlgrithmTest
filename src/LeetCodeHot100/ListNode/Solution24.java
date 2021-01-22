5716125a6b90c8171e827f2cf70e93ba2a8ee36c
542663467@qq.com <542663467@qq.com>
2021-01-22 16:47:38 +0800

    20210122

--- src/LeetCodeHot100/ListNode/Solution24.java
+++ src/LeetCodeHot100/ListNode/Solution24.java
@@ -53,9 +53,11 @@ public class Solution24 {
         return secondNode;
 
     }
-    class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) { val = x; }
-    }
+
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
